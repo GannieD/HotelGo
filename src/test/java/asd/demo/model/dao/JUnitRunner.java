304d2578fdb8a17fc8d11672191cab99f7757f01
Ziming Liu <liuzimingjay@vip.qq.com>
2019-09-16 07:37:18 +1000

    Unit Test

---
+++ src/test/java/asd/demo/model/dao/JUnitRunner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asd.demo.model.dao;
+import org.junit.runner.JUnitCore;
+/**
+ *
+ * @author liuziming
+ */
+public class JUnitRunner {
+    public static void main(String[] args){
+        System.out.println("------------TEST TEXT------------");
+    }
+}
